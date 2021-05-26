dcf080788682d9e9305ead5be83f074a16533abe
fmowl10 <jinseok1001@hotmail.com>
2021-05-26 20:01:51 +0900

    Add try catch

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "board.h"
+#include <iostream>
 #include <ncurses.h>
 
 using namespace std;
@@ -8,8 +9,17 @@ int main(int argc, char const *argv[])
     initscr();
     cbreak();
     refresh();
-    Board now = Board();
-    int result = now.loop();
-    endwin();
-    return result;
+    try
+    {
+        Board now = Board();
+        int result = now.loop();
+        endwin();
+        return result;
+    }
+    catch (BoardMiniumSizeException &e)
+    {
+        endwin();
+        cout << e.what() << endl;
+        return EXIT_FAILURE;
+    }
 }
